8b8b894a84f21733743b1194ee1e872e805c0376
Nikanos Polykarpou <nikanos@windowslive.com>
2020-07-01 00:13:31 +0300

    Completed module

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -1,12 +1,8 @@
 package com.h2;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    public static void main(String[] args )
     {
         System.out.println( "Hello World!" );
     }
--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class BestLoanRates {
     public static final Map<Integer,Float> bestRates=Map.of(1, 5.50f, 2, 3.45f, 3, 2.67f);
 
-    public static void main( String[] args )
+    public static void main(String[] args )
     {
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter your name");
